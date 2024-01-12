efd4f6d8b726c79cec50804d2129e79cd1ab104f
mamoud <109629935+0xmamoud@users.noreply.github.com>
2024-01-12 15:34:52 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
     bot: {
-        token:  "MTE5NTM2MzY1MDc2OTI2ODgwOA.GUfDLe.A7YoZNwvLdZd2n07gp6KuVSAtiVn8KYNMGNwys",
+        token:  proces.env.TOKEN,
         presence: []
     }
 }
